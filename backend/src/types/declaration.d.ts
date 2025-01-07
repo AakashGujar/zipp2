6a0ab6b148a501d37368868c55fa083db728ae67
aakashgujar <aakashgujar2001@gmail.com>
2025-01-07 08:47:48 +0530

    added declaration file

--- backend/src/types/declaration.d.ts
+++ backend/src/types/declaration.d.ts
@@ -1,53 +1,58 @@
-declare module '@amplitude/ua-parser-js' {
-    class UAParser {
-        constructor(uastring?: string, extensions?: any[]);
-        setUA(uastring: string): UAParser;
-        getResult(): {
-            ua: string;
-            browser: {
-                name?: string;
-                version?: string;
-                major?: string;
-            };
-            engine: {
-                name?: string;
-                version?: string;
-            };
-            os: {
-                name?: string;
-                version?: string;
-            };
-            device: {
-                vendor?: string;
-                model?: string;
-                type?: string;
-            };
-            cpu: {
-                architecture?: string;
-            };
-        };
-        getBrowser(): {
-            name?: string;
-            version?: string;
-        };
-        getDevice(): {
-            model?: string;
-            type?: string;
-            vendor?: string;
-        };
-        getEngine(): {
-            name?: string;
-            version?: string;
-        };
-        getOS(): {
-            name?: string;
-            version?: string;
-        };
-        getCPU(): {
-            architecture?: string;
-        };
-        getUA(): string;
-    }
-  
-    export default UAParser;
+declare module "@amplitude/ua-parser-js" {
+  class UAParser {
+    constructor(uastring?: string, extensions?: any[]);
+    setUA(uastring: string): UAParser;
+    getResult(): {
+      ua: string;
+      browser: {
+        name?: string;
+        version?: string;
+        major?: string;
+      };
+      engine: {
+        name?: string;
+        version?: string;
+      };
+      os: {
+        name?: string;
+        version?: string;
+      };
+      device: {
+        vendor?: string;
+        model?: string;
+        type?: string;
+      };
+      cpu: {
+        architecture?: string;
+      };
+    };
+    getBrowser(): {
+      name?: string;
+      version?: string;
+    };
+    getDevice(): {
+      model?: string;
+      type?: string;
+      vendor?: string;
+    };
+    getEngine(): {
+      name?: string;
+      version?: string;
+    };
+    getOS(): {
+      name?: string;
+      version?: string;
+    };
+    getCPU(): {
+      architecture?: string;
+    };
+    getUA(): string;
   }
+
+  export default UAParser;
+}
+
+declare module "pg";
+declare module "cookie-parser";
+declare module "qrcode";
+declare module "cors";
